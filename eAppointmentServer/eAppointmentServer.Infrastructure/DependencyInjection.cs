c75cc1e178dbcf1011047f627c22830ed8a9d48e
isayasin <isayasinkuru@gmail.com>
2024-10-04 15:47:00 +0300

    eAppointment Projesinin Backend tarafındaki Aplication, Domain ve Infrastructure Katmanları oluşturuldu.

---
+++ eAppointmentServer/eAppointmentServer.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,49 @@
+﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Infrastructure.Context;
+using GenericRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace eAppointmentServer.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+        });
+        services.AddIdentity<AppUser, AppRole>(action =>
+        {
+            action.Password.RequiredLength = 6;
+            action.Password.RequireLowercase = true;
+            action.Password.RequireUppercase = true;
+            action.Password.RequireNonAlphanumeric = true;
+            action.Password.RequireDigit = true;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddScoped<IUnitOfWork>(srv => srv.GetRequiredService<ApplicationDbContext>());
+
+        services.Scan(action =>
+        {
+            action
+            .FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(registrationStrategy: RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime();
+
+        });
+
+        /*services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+        services.AddScoped<IDoctorRepository, DoctorRepository>();
+        services.AddScoped<IPatientRepository, PatientRepository>();
+
+        services.AddScoped<IJwtProvider, IJwtProvider>();*/
+        return services;
+    }
+}
---
+++ eAppointmentServer/eAppointmentServer.Infrastructure/Services/JwtProvider.cs
@@ -0,0 +1,43 @@
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace eAppointmentServer.Infrastructure.Services;
+internal sealed class JwtProvider : IJwtProvider
+{
+    public string CreateToken(AppUser user)
+    {
+        List<Claim> claims =
+        [
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.FullName),
+            new(ClaimTypes.Email, user.Email ?? string.Empty),
+            new("UserName", user.UserName ?? string.Empty)
+        ];
+
+        DateTime expires = DateTime.Now.AddDays(1);
+
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(string.Join("-", Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid())));
+
+        SigningCredentials signinCredentials = new(securityKey, SecurityAlgorithms.HmacSha512);
+
+        JwtSecurityToken securityToken = new(
+            issuer: "Isa Yasin Kuru",
+            audience: "eAppointment",
+            claims: claims,
+            notBefore: DateTime.Now,
+            expires: expires,
+            signingCredentials: signinCredentials
+            );
+
+        JwtSecurityTokenHandler securityTokenHandler = new();
+
+        string token = securityTokenHandler.WriteToken(securityToken);
+
+        return token;
+
+    }
+}
