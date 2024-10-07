8b385c57bdf1a529cb1665d01447ef3a889f7e03
isayasin <isayasinkuru@gmail.com>
2024-10-07 19:58:32 +0300

    WebAPI katmanını oluşturduk. - Sql Connection'u ekleyip, migrationla database'imizi oluşturduk. - Cors Politikasını ekledik. - Login endpoint'imizi oluşturduk. - EndPointler için baseController oluşturduk

--- eAppointmentServer/eAppointmentServer.Application/obj/Debug/net8.0/eAppointmentServer.Application.AssemblyInfo.cs
+++ eAppointmentServer/eAppointmentServer.Application/obj/Debug/net8.0/eAppointmentServer.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eAppointmentServer.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b861fb8b9fc6db344fbd243c199f03496cad27")]
 [assembly: System.Reflection.AssemblyProductAttribute("eAppointmentServer.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eAppointmentServer.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- eAppointmentServer/eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
+++ eAppointmentServer/eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,6 +11,10 @@ internal sealed class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
         builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
         builder.Property(p => p.LastName).HasColumnType("varchar(50)");
         //builder.HasIndex(x => x.LastName).IsUnique();
+
+        builder.Property(p => p.Department)
+            .HasConversion(v => v.Value, v => DepartmentEnum.FromValue(v))
+            .HasColumnName("Department");
     }
 }
 
